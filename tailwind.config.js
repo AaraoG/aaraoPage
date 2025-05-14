389b3236b168f57b6494e32b24f19cbf229c0287
AaraoG <gabriel.mora.alvarado@gmail.com>
2025-05-14 15:37:09 -0600

    Index terminado

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './**/*.html', 
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'], 
+      },
+    },
+  },
+  plugins: [],
+};
